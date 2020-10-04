4f26857dd86bb6b2abe416f3e2f47cc7e87d83ca
Aref Mozafari <arefmozafari.78@gmail.com>
2020-10-04 06:23:09 +0330

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.0.1] - Initial release. TODO: add date: 
+
+* Initial release.
---
+++ lib/flutter_pw_validator.dart
@@ -0,0 +1,7 @@
+library flutter_pw_validator;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
